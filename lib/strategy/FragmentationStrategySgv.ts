9c600ae7e09e1405b58fba940d513eabe10197bd
Jitse De Smet <jitse@reedonk8.be>
2024-03-14 16:37:58 +0100

    working example

--- lib/strategy/FragmentationStrategySgv.ts
+++ lib/strategy/FragmentationStrategySgv.ts
@@ -10,34 +10,43 @@ const fs = require('node:fs');
 const DF = new DataFactory<RDF.Quad>();
 
 export class FragmentationStrategySgv extends FragmentationStrategyStreamAdapter {
-  private readonly fragmentationPredicate = DF.namedNode('http://localhost:3000/internal/postsFragmentation');
-  private readonly turtleSource = './sgv-descr.ttl';
+  private readonly fragmentationPredicate;
+  private readonly fragmentationObject;
 
-  private readonly turtleStore: Quad[] = [];
+  private readonly readPromise: Promise<Quad[]>;
 
-  private readonly readPromise = new Promise<Quad[]>((resolve, reject) => {
-    rdfParser.parse(fs.createReadStream(this.turtleSource), {
-      contentType: 'text/turtle',
-    }).on('data', (quad: Quad) => {
-      this.turtleStore.push(quad);
-    }).on('error', reject).on('end', () => resolve(this.turtleStore));
-  });
-
-  public constructor() {
+  public constructor(
+    private readonly sgvTemplatePath: string,
+    private readonly predicate: string,
+    private readonly object: string,
+    private readonly subjectRegexMatch: string,
+  ) {
     super();
+    const turtleStore: Quad[] = [];
+    this.fragmentationPredicate = DF.namedNode(predicate);
+    this.fragmentationObject = DF.namedNode(object);
+    this.readPromise = new Promise<Quad[]>((resolve, reject) => {
+      rdfParser.parse(fs.createReadStream(sgvTemplatePath), {
+        contentType: 'text/turtle',
+      }).on('data', (quad: Quad) => {
+        turtleStore.push(quad);
+      }).on('error', reject).on('end', () => resolve(turtleStore));
+    });
   }
 
   public async handleQuad(quad: Quad, quadSink: IQuadSink): Promise<void> {
-    if (quad.predicate.equals(this.fragmentationPredicate)) {
+    if (quad.predicate.equals(this.fragmentationPredicate) && quad.object.equals(this.fragmentationObject)) {
       const quads = await this.readPromise;
 
-      const subjectIri = quad.subject.value.replace(/profile\/card#me$/u, 'sgv');
+      const subjectIri = quad.subject.value.replace(new RegExp(this.subjectRegexMatch, 'u'), 'sgv');
 
       for (const turtleQuad of quads) {
         await quadSink.push(
           subjectIri,
           DF.quad(
-            DF.namedNode(subjectIri + turtleQuad.subject.value),
+            turtleQuad.subject.termType === 'BlankNode' ?
+              turtleQuad.subject :
+              DF.namedNode(subjectIri + turtleQuad.subject.value),
             turtleQuad.predicate,
             turtleQuad.object,
             turtleQuad.graph,
